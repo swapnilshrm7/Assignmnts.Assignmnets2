fcb035ecd5ca966d274027d21441f686378c9ff5
swapnil <swapnilshrm7@gmail.com>
2018-07-26 13:05:04 +0530

    Initail commit for assignment2

---
+++ EmployeeAssignment.Entity/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAssignment.Entity
+{
+    public class Employee
+    {
+        private string _name;
+        public string Name {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int MyProperty { get; set; }
+
+
+    }
+}
